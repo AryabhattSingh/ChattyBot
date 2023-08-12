154e5e6ff45699771cfbee467556b50317c7d42c
Aryabhatt <bhattsahab314@gmail.com>
2023-08-12 07:58:10 +0530

    Removed some blank linkes

--- src/chattybot/MainClass.java
+++ src/chattybot/MainClass.java
@@ -3,17 +3,13 @@ package chattybot;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
 		ChattyBot bot = new ChattyBot();
-		
 		bot.introMessage();
 		bot.askUserName();
 		bot.predictAge();
 		bot.countNumbers();
 		bot.testProgrammingKnowledge();
 		bot.outroMessage();
-		
-
 	}
 
 }
